b5467ab0c2b85a0020e893cd44b7fd67083d4754
fastop <51775982+fastop@users.noreply.github.com>
2022-11-30 16:12:57 -0800

    Added and rename

---
+++ 7_Funciones.py
@@ -0,0 +1,24 @@
+#Ciclos FOR
+
+
+print("My Nombre es:")
+for i in range(5):
+    print ("LE MEMEASTER ("+str(i)+")");
+
+ 
+# Segunda parte -> STEPS for RANGE
+print("---------------")
+for i in range(12, 16):
+    print(i)
+
+
+# Tercera parte -> STEPS for RANGE
+print("---------------")
+for i in range(0, 10, 2):
+    print(i)
+
+
+# Cuarta parte -> STEPS for RANGE
+print("---------------")
+for i in range(5, -1, -1):
+    print(i)
